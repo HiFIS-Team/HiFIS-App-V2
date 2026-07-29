afc0279ce64cc6a24e140b4914edbce6849c3dac
김은후 <eunhoo0903@naver.com>
2026-07-29 18:32:54 +0900

    feat: 런치 스크린과 앱 전환 화면에 마크 표시
    - iOS 런치 이미지를 케틀벨 마크로 교체
    - 앱 비활성 시 마크 가림막을 덮어 전환 화면에서 내용 숨김
    - 복귀 시 가림막 자동 해제

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,34 @@ void main() {
   runApp(HiFISApp());
 }
 
-class HiFISApp extends StatelessWidget {
+class HiFISApp extends StatefulWidget {
   HiFISApp({super.key});
+
+  @override
+  State<HiFISApp> createState() => _HiFISAppState();
+}
+
+class _HiFISAppState extends State<HiFISApp> with WidgetsBindingObserver {
+  /// 앱이 비활성 상태(앱 전환 화면 등)일 때 화면을 가릴지 여부
+  bool _obscured = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    final obscured = state != AppLifecycleState.resumed;
+    if (obscured != _obscured) setState(() => _obscured = obscured);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +47,25 @@ class HiFISApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.current,
       home: _SplashGate(),
+      // 앱 전환 화면(멀티태스킹)에서는 내용 대신 마크만 보이도록
+      // 비활성 상태에 가림막을 덮는다 (토스식 프라이버시 커버)
+      builder: (context, child) => Stack(
+        children: [
+          child!,
+          if (_obscured)
+            Positioned.fill(
+              child: Container(
+                color: AppColors.surface,
+                alignment: Alignment.center,
+                child: Image.asset(
+                  'assets/images/hifis_mark.png',
+                  height: 96,
+                  cacheHeight: 288,
+                ),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
